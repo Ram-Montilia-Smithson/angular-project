769036944f65e0584827ff62a28f7e16ea0cebe2
Ram-Montilia-Smithson <ramsmithson1@gmail.com>
2022-02-01 18:42:11 +0200

    connecting between shopping item and shopping detail to show the details of the selected recipe item, and adding the functionality to the add button, inside the shopping edit component
    adding Form Module to app module imports.
    
    adding a click event to recipe item component that in turn emitting onSelectRecipe event that is calling sendRecipe function with the recipeElement as an argument.
    in turn, sendRecipe function is emitting onRecipeSent event with the recipe chosen as an argument which is assigned to the selectedRecipe variable inside recipes component.
    
    adding *ngIf to app-recipe-detail based on weather selectedRecipe is assigned a value or not, and if not (else) it renders the infoText ng-template with some text.
    if selectedRecipe is assigned a value, it is passed to  app-recipe-detail as recipe and it's values are being presented instead of the hard coded text.
    
    adding a references to both name input and amount input and adding a click event to the add button inside shopping-edit.component template, that calls addIngredient function which, in turn, emitting the onIngredientAddition event with the values from the input references as a new Ingredient type which is passed to addingToIngredients function inside shopping-list.component that adds the new ingredient to the ingredients array.

--- angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,16 +6,18 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent {
+
+  @Output() onRecipeSent = new EventEmitter<Recipe>()
+
   recipes: Recipe[] = [
+    new Recipe('Example Recipe', 'Example description', 'https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466__340.jpg'),
     new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('Another Test Recipe', 'Another option to choose from', 'https://cdn.pixabay.com/photo/2016/02/02/15/33/dishes-1175493_960_720.jpg')
   ]
 
-  constructor() { }
-
-  ngOnInit(): void {
+  sendRecipe(recipe: Recipe) {
+    this.onRecipeSent.emit(recipe)
   }
 
 }
--- angular-project/src/app/recipes/recipes.component.ts
+++ angular-project/src/app/recipes/recipes.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent implements OnInit {
+export class RecipesComponent {
 
-  constructor() { }
+  // instead of showing this commented default recipe at app start, I choose to show the infoText like in the video example
+  // selectedRecipe: Recipe = new Recipe('The Default Recipe', 'Shown at app start', 'https://cdn.pixabay.com/photo/2015/12/20/17/11/fish-1101436_960_720.jpg')
+  selectedRecipe: Recipe 
 
-  ngOnInit(): void {
-  }
+  // selectRecipe(recipe: Recipe) {
+  //   this.selectedRecipe = recipe
+  // }
 
 }
